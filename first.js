41f8e8e4a2a85aaafb92448fb930221270165229
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-07-26 22:07:08 +0530

    switch case

--- first.js
+++ first.js
@@ -9,19 +9,38 @@
 
 //Conditional Statement
 
-let age = 25;
+// let age = 25;
 
-if(age < 18) {
-    console.log("junior");
-}
+// if(age < 18) {
+//     console.log("junior");
+// }
 
-else if (age > 60){
-    console.log("senior");
-}
+// else if (age > 60){
+//     console.log("senior");
+// }
 
-else{
-    console.log("Middle");
+// else{
+//     console.log("Middle");
+// }
+
+const foo = 0;
+switch (foo) {
+  case -1:
+    console.log("negative 1");
+    break;
+  case 0: // Value of foo matches this criteria; execution starts from here
+    console.log(0);
+  // Forgotten break! Execution falls through
+  case 1: // no break statement in 'case 0:' so this case will run as well
+    console.log(1);
+    break; // Break encountered; will not continue into 'case 2:'
+  case 2:
+    console.log(2);
+    break;
+  default:
+    console.log("default");
 }
+// Logs 0 and 1
 
 
 
