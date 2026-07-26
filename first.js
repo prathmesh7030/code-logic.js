b27a9e87de48cc7f241f10ce0e0b1adc6252e964
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-07-26 19:45:39 +0530

    conditional statement

--- first.js
+++ first.js
@@ -1,10 +1,22 @@
-const product = {
-    title: "Ball pen",
-    rating: 4,
-    offer: 5,
-    price: 270
-};
+// const product = {
+//     title: "Ball pen",
+//     rating: 4,
+//     offer: 5,
+//     price: 270
+// };
 
-console.log(product);
+// console.log(product);
+
+//Conditional Statement
+
+let age = 25;
+
+if (age >= 18){
+    console.log("you can vote")
+}
+
+if(age < 18){
+    console.log("you cannot vote")
+}
 
 
