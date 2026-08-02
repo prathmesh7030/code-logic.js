fdbd511b65bb0351f36723da71a368aa677b5826
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-02 23:06:38 +0530

    Append text to original text

--- first.js
+++ first.js
@@ -1,2 +1,5 @@
-let div = document.querySelector("div");
-console.dir(div);
+let h2 = document.querySelector("h2");
+
+console.dir(h2.innerText);
+
+h2.innerText = h2.innerText + " from local classes"
