64557a24efdbff09c08bca58fc23b35fc5260382
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-04 15:44:33 +0530

    removing event handlers

--- first.js
+++ first.js
@@ -1,10 +1,23 @@
 let btn1 = document.querySelector("#btn1");
 
-btn1.addEventListener("click", (evt) => {
-    console.log("button1 was clicked");
-    console.log(evt);
-    console.log(evt.type);
+btn1.addEventListener("click", () => {
+    console.log("button1 was clicked - Handler 1");
 });
 
-let div = document.querySelector("div");
+btn1.addEventListener("click", () => {
+    console.log("button1 was clicked - Handler 2");
+});
 
+
+const handler3 =() => {
+    console.log("button1 was clicked - Handler3");
+};
+
+btn1.addEventListener("click", handler3);
+
+btn1.removeEventListener("click", handler3);
+
+
+btn1.addEventListener("click", () => {
+    console.log("button1 was clicked - Handler 4");
+});
