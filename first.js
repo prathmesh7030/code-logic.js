84cb75013d077580aa6cbc3c19b5adb5a468cf9c
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-04 16:39:57 +0530

    created button to change the theme

--- first.js
+++ first.js
@@ -1,13 +1,16 @@
 let modebtn = document.querySelector("#mode");
+let body = document.querySelector("body")
 let currMode = "light";
 
 modebtn.addEventListener("click", () => {
     if (currMode === "light"){
         currMode = "dark";
-        document.querySelector("body").style.backgroundColor = "black";
+        body.classList.add("dark");
+        body.classList.remove("light");
     }else{
         currMode = "light";
-        document.querySelector("body").style.backgroundColor = "white";
+        body.classList.add("light");
+        body.classList.remove("dark");
     }
     console.log(currMode);
 });
