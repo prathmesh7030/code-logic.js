8d59c8171414981114e04ffcb1adbba48ebed54b
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-13 22:07:34 +0530

    Nested callback

--- first.js
+++ first.js
@@ -1,9 +1,22 @@
-console.log("One");
-console.log("two");
+function getData(dataId, getNextData) {
 
-setTimeout(() => {
-   console.log("Hello"); 
-}, 2000); // Timeout ;
+    setTimeout( () => {
+        console.log("data", dataId);
+        if(getNextData) {
+            getNextData();
+        }
+    } ,2000);
+}
 
-console.log("Three");
-console.log("Four");
+getData(1, () => {
+    console.log("Getting data 2 ....");
+    getData(2, () => {
+        console.log("Getting data 3 ....");
+        getData(3, () => {
+            console.log("Getting data 4 ....");
+            getData(4, () => {
+
+            })
+        })
+    })
+})
