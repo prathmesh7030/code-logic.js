ee51f67cbc0c0e9780a0b8a1c529a03e4cec04b0
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-13 21:28:03 +0530

    timeout function

--- first.js
+++ first.js
@@ -1,17 +1,6 @@
-let a = 5;
-let b = 10;
-console.log("a = ", a);
-console.log("b = ", b);
-console.log("a + b = ", a + b);
-
-try{
-console.log("a + b = ", a + c); //error
-
-}catch(err) {
-    console.log(err);
+function hello () {
+    console.log("hello");
 }
 
-console.log("a + b = ", a + b);
-console.log("a + b = ", a + b);
-console.log("a + b = ", a + b);
-console.log("a + b = ", a + b);
+
+setTimeout(hello, 2000); //Timeout ;
