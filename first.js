9bb93cce1339f8c9e076bcf1fee603abbef6a758
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-19 21:06:59 +0530

    Resolve promice

--- first.js
+++ first.js
@@ -1,12 +1,13 @@
-let promice = new promice((resolve, reject) => {
-    console.log("I am a promice");
-    reject("Some error occupied");
-});
+// let promice = new promice((resolve, reject) => {
+//     console.log("I am a promice");
+//     reject("Some error occupied");
+// });
 
 function getData(dataId, getNextData) {
     return new promice((resolve, reject) => {
         setTimeout(() =>{
             console.log("data", dataId);
+            resolve("Success");
             if (getNextData) {
                 getNextData();
             }
